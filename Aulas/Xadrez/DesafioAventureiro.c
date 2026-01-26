237c93ef5331a6b2694f9e9e2d58ecbab7706be2
gtito7 <gabriel.tito@hotmail.com>
2026-01-26 16:59:48 -0300

    Desafio - Xadrez - Aventureiro

--- Aulas/Xadrez/DesafioAventureiro.c
+++ Aulas/Xadrez/DesafioAventureiro.c
@@ -72,5 +72,50 @@ int main() {
         casasRainha++;
     } while (casasRainha <= Rainha);
 
+     printf("\n");
+
+    // CAVALO (Loop Aninhado)
+    int direcaoCavalo;
+    int movimentoCompleto = 1;
+
+    printf("## CAVALO ##\n");
+    printf("Escolha uma direção:\n");
+    printf("1-Cima, Direita\t\t");
+    printf("2-Cima, Esquerda\n");
+    printf("3-Baixo, Direita\t");
+    printf("4-Baixo, Esquerda\n");
+    printf("5-Esquerda, Baixo\t");
+    printf("6-Esquerda, Cima\n");
+    printf("7-Direita, Baixo\t");
+    printf("8-Direita, Cima\n");
+    scanf("%d", &direcaoCavalo);
+
+    while (movimentoCompleto--) {
+
+    //Primeiro Movimento
+        for (int i = 0; i < 2; i++) {
+            if (direcaoCavalo == 1 || direcaoCavalo == 2)
+                printf("Cima\n");
+            else if (direcaoCavalo == 3 || direcaoCavalo == 4)
+                printf("Baixo\n");
+            else if (direcaoCavalo == 5 || direcaoCavalo == 6)
+                printf("Esquerda\n");
+            else if (direcaoCavalo == 7 || direcaoCavalo == 8)
+                printf("Direita\n");
+        }
+
+    //Segundo Movimento
+        if (direcaoCavalo == 1 || direcaoCavalo == 3)
+            printf("Direita\n");
+        else if (direcaoCavalo == 2 || direcaoCavalo == 4)
+            printf("Esquerda\n");
+        else if (direcaoCavalo == 5 || direcaoCavalo == 7)
+            printf("Baixo\n");
+        else if (direcaoCavalo == 6 || direcaoCavalo == 8)
+            printf("Cima\n");
+    }
+
+
     return 0;
+    
 }
