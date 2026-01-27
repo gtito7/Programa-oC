bfa1df0009892b63a79731587cffd75427336fd4
gtito7 <gabriel.tito@hotmail.com>
2026-01-27 18:08:54 -0300

    Vetores e Matrizes - Atualização

---
+++ Aulas/VetoresMatrizes/hora_de_codar3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define LINHA 10
+#define COLUNA 10
+
+int main(){
+
+int matriz[LINHA][COLUNA];
+int target = 90;
+int found = 0;
+int soma = 1;
+
+for (int i = 0; i < LINHA; i++)
+{
+    for(int j = 0; j < COLUNA; j++)
+    {
+        matriz[i][j] = soma;
+        soma++;
+        printf("%d\t", matriz[i][j]);
+    }
+    printf("\n");
+}
+
+// Busca pelo elemento
+for(int i = 0; i < LINHA; i++)
+{
+    for(int j = 0; j < COLUNA; j++)
+    {
+        if (matriz[i][j] == target)
+        {
+        printf("O valor %d foi encontrado no indice (%d, %d)\n", target, i, j);
+        found = 1;
+        break;
+        }
+    }
+    if (found) break;
+}
+    
+if (!found)
+{
+    printf("Elemento %d não encontrado!\n", target);
+}
+
+    return 0;
+}
